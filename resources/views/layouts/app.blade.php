068c1628443fa48f93b1553b5245545a2b0f5e17
Alan <alanseau@gmail.com>
2019-08-08 11:16:49 -0400

    updated version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Just Check In') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js?v=1.02') }}" defer></script>
+    <script src="{{ asset('js/app.js?v=1.03') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
